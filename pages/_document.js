77c88216e5c177c13fa5e39dadaa14d4e4fd26a5
shakeybuoy <abhi.sharma.0873@gmail.com>
2023-04-28 19:07:26 +0530

    banner img

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:image" content="/banner.webp" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/webp" />
+      </Head>
       <body>
         <Main />
         <NextScript />
